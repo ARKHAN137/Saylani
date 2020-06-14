7b06021e3835cb27e401d55c884ba41f75a82ddf
Abdur Rehman Khan <arkhanking137@gmail.com>
2020-06-14 07:00:31 -0700

    MY saylnai assignment

---
+++ app.js
@@ -0,0 +1,121 @@
+// //Chapter #1 task #1
+//prompt("Enter the password")
+//alert("Error! please enter a valid pasword");
+
+
+
+
+// //Chapter #1 task #2
+// alert("Welcome to JS Land.....\nHappy coding!")
+
+
+// //Chapter #1 task #3
+// alert("Welcome to JS Land...")
+// alert("Happy Coding! \n Prevent this page from creating additional dialogs")
+
+// //Chapter #1 task #2
+// var a= ("Hello... I can run JS throgh my web browser's console")
+// console.log(a)
+
+
+
+// //Chapter #2 task #1
+
+
+// prompt("Enter your name","username")
+// //Chapter #2 task #2
+// prompt("myName","Full Name")
+
+// //Chapter #2 task #3
+// a= prompt ("Write a messege","Hello world")
+// alert(a)
+
+
+
+
+
+
+// //Chapter #2 task #4
+// //  b= prompt ("Find Student")
+// //     alert(b)
+// var student;
+// if (b = prompt("Find Student")) {
+//     alert(b)
+//     student = (confirm("19 Years old"))
+// } else {
+//     alert("Prosses cancel!")
+
+// }
+// alert ("Certified Mobile Application Development")
+
+
+
+
+
+// //Chapter #2 task #5
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+
+
+
+
+
+// //Chapter #2 task #6
+// //Out of understanding sir
+
+
+
+
+
+
+// //Chapter #2 task #7
+// if(prompt("Book","write 'A' to see the message")){
+// alert("I am trying to learn from the Book A smarter way to learn JavaScript")
+// }else{
+//     alert("Your type invilid")
+// }
+
+
+
+
+// //Chapter #2 task #8
+// document.write("Yah! I can write HTML content through javaScript")
+
+
+
+
+// //Chapter #2 task #9
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+
+
+
+//Chapter #3 task #1
+// var age = prompt("Enter your age")
+// alert("I am " + age + " Year old")
+
+
+
+//Chapter #3 task #2
+
+// var visit= prompt("How Many time you visited this site","Enter Your Respons" )
+// alert("You have Visited this sit " + visit + " times")
+
+
+//Chapter #3 task #3
+
+
+// var birth= prompt("Enter your Birthday Year", "Enter year")
+// document.write("My Birth year is " + birth)
+
+
+
+//Chapter #3 task #4
+// var store = prompt("Enter your name")
+// var web = prompt("Enter Website name to shopping")
+// var product = prompt("Whats you want an oder ", "Your oder")
+// var quan = prompt("How many Quantity Your items" , "Enter Respons")
+// document.write(web  + quan + product +store)
+
+
+
+
+//Chapter #4 task #1
